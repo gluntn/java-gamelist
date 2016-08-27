dca3019eda625b8796fad43c065883aa509167a3
William Østensen <William stensen>
2016-08-27 18:26:58 +0200

    delete (somewhat) works now, search works good

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils;
 import java.util.Scanner;
+
+/*
+ * TODO: Add a wait to edit
+ * TODO: Make it so that delete actually deletes the element
+ * TODO: Make the show function a little prettier
+ * TODO: GUI elements eventually?
+ * 	TODO: Fetch images from the web to show the game-cover
+ * 
+ */
 
 public class Main {
 
@@ -12,28 +22,41 @@ public class Main {
 		
 		System.out.println("Welcome to the Game Library!");
 		
-		while(!finished || command == "exit") {
+		mainLoop: while(!finished || command == "exit") {
 			System.out.println("What would you like to do? \n"
 					+ "(add) new game, (delete) game, (show) list, (edit) game");
-			command = input.next();
+			command = input.nextLine();
 			
 			switch(command) {
 			case "add":
 				// Why does Game and Publisher come out the same time
 				System.out.println("Game: ");
-				String game = parameter.nextLine();
+				String game = parameter.next();
 				System.out.println("Publisher: ");
-				String publisher = parameter.nextLine();
+				String publisher = parameter.next();
 				System.out.println("Published year: ");
 				int age = parameter.nextInt();
 				games.add(new GameStorage(game, publisher, age));
 				break;
 			case "delete":
 				// how the heck do i do this
+				// First of all, get the query
 				String query = parameter.nextLine();
+				// Find all the elements that matches the query
 				ArrayList<GameStorage> gameList = findElement(games, query);
-				for(GameStorage ge : gameList) {
-					System.out.println(ge.getName());
+				for(int i = 0; i < gameList.size(); i++) {
+					// Print out the list and its corresponding index numbers
+					System.out.println((i+1) + " " + gameList.get(i).getName());
+				}
+				System.out.println("\nWhich number would you like to delete?");
+				int deleteAt = parameter.nextInt();
+				// Make sure there are no illegal selections
+				if(deleteAt <= 0 || deleteAt > gameList.size()) System.out.println("Nothing there!");
+				else {
+					// Is this the best practice?
+					String tempVal = gameList.get(deleteAt - 1).getName();
+					gameList.remove(deleteAt - 1);
+					System.out.println(tempVal + " is deleted!");
 				}
 				break;
 			case "show":
@@ -44,7 +67,7 @@ public class Main {
 				break;
 			case "exit":
 				System.out.println("quitting...");
-				break;
+				break mainLoop;
 			default:
 				System.out.println("what");
 				break;
@@ -72,7 +95,8 @@ public static ArrayList<GameStorage> findElement(ArrayList<GameStorage> gameList
 	for(int i = 0; i < gameList.size(); i++) {
 		// Check if the given query is equal to the current element
 		// Java is at the given moment
-		if((gameList.get(i).getName()).equalsIgnoreCase(query)) {
+		//if((gameList.get(i).getName()).equalsIgnoreCase(query)) 
+		if(StringUtils.containsIgnoreCase(gameList.get(i).getName(), query)){
 			// If the gameList is empty, say that it didn't find anything
 			// and break the loop
 			if(gameList.size() == 0) {
